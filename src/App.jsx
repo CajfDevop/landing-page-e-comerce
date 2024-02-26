cc73b6e40e62a86dad8cf2ebf8d022064cb0f073
jaracar84 <cajfdeveloper@gmail.com>
2024-02-26 12:10:45 -0500

    file create | First commit for this project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='bg-red-100'>
+        Hola mundito
+      </div>
+  )
+}
+
+export default App
